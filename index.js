45de5648b62d3bea04e3aa20d20bc244e1f3cbdb
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-23 17:24:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const kittens = ["Milo", "Otis", "Garfield"]
+var name = ["Ralph", "Bob", "Broom", "Arnold"]
+
+function destructivelyAppendKitten(kittens, name[0]){
+    kittens.push(name[0])
+    return kittens
+}
