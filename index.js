74311fb583ba11af70475ce370cf1b58619adb02
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-23 17:31:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ function destructivelyAppendKitten(kittens, name[0]){
     kittens.push(name[0])
     return kittens
 }
+
+function destructivelyPrependKitten(kittens, name[1]){
+  kittens.unshift(name[1])
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(kittens){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(kittens){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(kittens, name[2]){
+  return appendKitten(name[2])
+}
+
+function prependKitten(kittens, name[3]){
+  return prependKitten(name[3])
+}
